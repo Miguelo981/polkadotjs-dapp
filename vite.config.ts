9f9cc42381ca382d4dd99d16b64214106efb67ed
Miguelo981 <55580212+Miguelo981@users.noreply.github.com>
2023-07-11 12:52:36 +0200

    Add project config files

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  /* server: {
+    hmr: {
+      overlay: false
+    }
+  }, */
+  plugins: [
+    svelte(),
+    nodePolyfills({
+      exclude: [
+        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
+  ],
+})
