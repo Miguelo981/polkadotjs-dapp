482736cffe1f09b81fec6e547fdcd8448eb5c8c6
Miguelo981 <55580212+Miguelo981@users.noreply.github.com>
2023-07-11 13:59:58 +0200

    Add base frontend

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import AccountList from "./lib/AccountsList.svelte";
+  import Balance from "./lib/Balance.svelte";
+  import ConnectionModal from "./lib/ConnectionModal.svelte";
+  import TransferForm from "./lib/TransferForm.svelte";
+</script>
+
+<main>
+  <ConnectionModal />
+  <section class="grid justify-items-center items-center h-screen w-screen">
+    <div class="flex flex-col items-center space-y-12">
+      <Balance />
+      <!-- <AccountList /> -->
+      <TransferForm />
+    </div>
+  </section>
+</main>
